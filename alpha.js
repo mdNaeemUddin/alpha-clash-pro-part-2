d180a2fdf586171177e65195644c4815ea11f056
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-02-17 02:37:02 +0600

    module practice done

--- alpha.js
+++ alpha.js
@@ -8,6 +8,9 @@
 // }
 
 
+
+
+
 function addElement(elementId) {
     const item = document.getElementById(elementId);
     item.classList.add('hidden');
@@ -16,6 +19,27 @@ function addElement(elementId) {
 function removeElement(elementId) {
     const item = document.getElementById(elementId);
     item.classList.remove('hidden');
+}
+
+function setBackground(elementId) {
+    const bg = document.getElementById(elementId);
+    bg.classList.add('bg-orange-400');
+}
+
+function removeBackgroundColor(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function randomCharacter() {
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    alphabetString.toUpperCase();
+
+    const alphabetArray = alphabetString.split('');
+    const index = Math.round(Math.random() * 25);
+    const ch = alphabetArray[index];
+    
+    return ch;
 }
 
 function continueGame() {
@@ -29,19 +53,83 @@ function continueGame() {
 
 }
 
-function setBackground(elementId) {
-    const bg = document.getElementById(elementId);
-    bg.classList.add('bg-orange-400');
+
+function getTextElementValue(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
 }
 
-function randomCharacter() {
-    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
-    alphabetString.toUpperCase();
+function setTextElementById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 
-    const alphabetArray = alphabetString.split('');
-    const index = Math.round(Math.random() * 25);
-    const ch = alphabetArray[index];
+}
+
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+
+}
+
+function handleButton(event) {
+    const playerPressed = event.key;
+    // console.log('player pressed : ', playerPressed);
+    console.log(playerPressed);
+
+    if (playerPressed === 'Enter') {
+        gameOver();
+    }
+
+    const currentAlphabetElement = document.getElementById('char');
+    const currentAlphabet = currentAlphabetElement.innerText;
     
-    return ch;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+  
+
+    if (playerPressed === expectedAlphabet) {
+        // console.log("you get a point");
+        removeBackgroundColor(expectedAlphabet);
+
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+        const currentScore = getTextElementValue('current-score');
+
+        const updateScore = currentScore + 1;
+
+        setTextElementById('current-score', updateScore);
+
+        // currentScoreElement.innerText = newScore;
+
+        continueGame();
+    } else {
+
+        const currentLife = getTextElementValue('current-life');
+        const updated = currentLife - 1;
+
+        setTextElementById('current-life', updated);
+
+       
+        if (updated === 0) {
+            gameOver();
+        }
+
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // const newLife = currentLife - 1;
+        // currentLifeElement.innerText = newLife;
+    }
+
+
+    // console.log(currentAlphabet.innerText);
 }
+
+document.addEventListener('keyup', handleButton);
 
--- utility.js
+++ utility.js
@@ -1,7 +1,26 @@
 
 
+
+
+
 function playNow() {
     addElement('play-now');
     removeElement('play-ground');
+    addElement('final-score');
+
+    setTextElementById('current-life', 5);
+    setTextElementById('current-score', 0);
     continueGame();
+}
+
+function gameOver() {
+    addElement('play-ground');
+    removeElement('final-score');
+
+    const lastScore = getTextElementValue('current-score');
+    setTextElementById('game-score', lastScore);
+
+    const currentAlphabet = getElementTextById('char');  
+    removeBackgroundColor(currentAlphabet);
+
 }
